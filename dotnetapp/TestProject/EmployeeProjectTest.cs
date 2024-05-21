fe44c9c567fd6c0b02bf5ec25972bdfd6f4f8999
Dhinesh.Kumar <Dhinesh.Kumar@stackroute.in>
2024-05-21 13:19:40 +0530

    testnew

---
+++ dotnetapp/TestProject/EmployeeProjectTest.cs
@@ -0,0 +1,38 @@
+﻿using dotnetapp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestFixture]
+    internal class EmployeeProjectTest
+    {
+        private ApplicationDbContext _context;
+        private HttpClient _httpClient;
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "TestDatabase").Options;
+            _context = new ApplicationDbContext(options);
+
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:8080");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+
+        private void ClearDatabase()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+        }
+
+    }
+}
---
+++ dotnetapp/TestProject/EmployeeProposalTest.cs
@@ -0,0 +1,152 @@
+﻿using dotnetapp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestFixture]
+    internal class EmployeeProposalTest
+    {
+        private ApplicationDbContext _context;
+        private HttpClient _httpClient;
+        
+        
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "TestDatabase").Options;
+            _context = new ApplicationDbContext(options);
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:8080");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+        //        1.	Backend_Test_Post_Method_Proposal_Employee
+        //2.	Backend_Test_Get_Method_Proposal_Employee
+        //3.	Backend_Test_Put_Method_Proposal_Employee
+        //4.	Backend_Test_Delete_Method_Proposal_Employee
+        [Test, Order(1)]
+        public async Task Backend_Test_Post_Method_Proposal_Employee() {
+            ClearDatabase();
+
+            string uniqueId = Guid.NewGuid().ToString();
+
+            // Generate a unique userName based on a timestamp
+            string uniqueUsername = $"abcd_{uniqueId}";
+            string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+            string requestBody = $"{{\"UserName\": \"{uniqueUsername}\", \"Password\": \"abc@123A\", \"Email\": \"{uniqueEmail}\", \"MobileNumber\": \"1234567890\", \"Role\": \"Employee\",\"ProfileImage\":\"employeeimage\"}}";
+            HttpResponseMessage response = await _httpClient.PostAsync("/user/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+            // Print registration response
+            string registerResponseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Registration Response: " + registerResponseBody);
+
+            // Login with the registered user
+            string loginRequestBody = $"{{\"Email\" : \"{uniqueEmail}\",\"Password\" : \"abc@123A\"}}"; // Updated variable names
+            HttpResponseMessage loginResponse = await _httpClient.PostAsync("/user/login", new StringContent(loginRequestBody, Encoding.UTF8, "application/json"));
+
+            // Print login response
+            string loginResponseBody = await loginResponse.Content.ReadAsStringAsync();
+            Console.WriteLine("Login Response: " + loginResponseBody);
+
+            Assert.AreEqual(HttpStatusCode.OK, loginResponse.StatusCode);
+
+        }
+        [Test]
+        public async Task Backend_Test_Get_Method_Proposal_Employee() {
+            ClearDatabase();
+
+            string uniqueId = Guid.NewGuid().ToString();
+
+            // Generate a unique userName based on a timestamp
+            string uniqueUsername = $"abcd_{uniqueId}";
+            string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+            string requestBody = $"{{\"UserName\": \"{uniqueUsername}\", \"Password\": \"abc@123A\", \"Email\": \"{uniqueEmail}\", \"MobileNumber\": \"1234567890\", \"Role\": \"Employee\",\"ProfileImage\":\"employeeimage\"}}";
+            HttpResponseMessage response = await _httpClient.PostAsync("/user/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+            // Print registration response
+            string registerResponseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Registration Response: " + registerResponseBody);
+
+            // Login with the registered user
+            string loginRequestBody = $"{{\"Email\" : \"{uniqueEmail}\",\"Password\" : \"abc@123A\"}}"; // Updated variable names
+            HttpResponseMessage loginResponse = await _httpClient.PostAsync("/user/login", new StringContent(loginRequestBody, Encoding.UTF8, "application/json"));
+
+            // Print login response
+            string loginResponseBody = await loginResponse.Content.ReadAsStringAsync();
+            Console.WriteLine("Login Response: " + loginResponseBody);
+
+            Assert.AreEqual(HttpStatusCode.OK, loginResponse.StatusCode);
+        }
+        [Test]
+        public async Task Backend_Test_Put_Method_Proposal_Employee() {
+            ClearDatabase();
+
+            string uniqueId = Guid.NewGuid().ToString();
+
+            // Generate a unique userName based on a timestamp
+            string uniqueUsername = $"abcd_{uniqueId}";
+            string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+            string requestBody = $"{{\"UserName\": \"{uniqueUsername}\", \"Password\": \"abc@123A\", \"Email\": \"{uniqueEmail}\", \"MobileNumber\": \"1234567890\", \"Role\": \"Employee\",\"ProfileImage\":\"employeeimage\"}}";
+            HttpResponseMessage response = await _httpClient.PostAsync("/user/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+            // Print registration response
+            string registerResponseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Registration Response: " + registerResponseBody);
+
+            // Login with the registered user
+            string loginRequestBody = $"{{\"Email\" : \"{uniqueEmail}\",\"Password\" : \"abc@123A\"}}"; // Updated variable names
+            HttpResponseMessage loginResponse = await _httpClient.PostAsync("/user/login", new StringContent(loginRequestBody, Encoding.UTF8, "application/json"));
+
+            // Print login response
+            string loginResponseBody = await loginResponse.Content.ReadAsStringAsync();
+            Console.WriteLine("Login Response: " + loginResponseBody);
+
+            Assert.AreEqual(HttpStatusCode.OK, loginResponse.StatusCode);
+        }
+        [Test]
+        public async Task Backend_Test_Delete_Method_Proposal_Employee() {
+            ClearDatabase();
+
+            string uniqueId = Guid.NewGuid().ToString();
+
+            // Generate a unique userName based on a timestamp
+            string uniqueUsername = $"abcd_{uniqueId}";
+            string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+            string requestBody = $"{{\"UserName\": \"{uniqueUsername}\", \"Password\": \"abc@123A\", \"Email\": \"{uniqueEmail}\", \"MobileNumber\": \"1234567890\", \"Role\": \"Employee\",\"ProfileImage\":\"employeeimage\"}}";
+            HttpResponseMessage response = await _httpClient.PostAsync("/user/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+            // Print registration response
+            string registerResponseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Registration Response: " + registerResponseBody);
+
+            // Login with the registered user
+            string loginRequestBody = $"{{\"Email\" : \"{uniqueEmail}\",\"Password\" : \"abc@123A\"}}"; // Updated variable names
+            HttpResponseMessage loginResponse = await _httpClient.PostAsync("/user/login", new StringContent(loginRequestBody, Encoding.UTF8, "application/json"));
+
+            // Print login response
+            string loginResponseBody = await loginResponse.Content.ReadAsStringAsync();
+            Console.WriteLine("Login Response: " + loginResponseBody);
+
+            Assert.AreEqual(HttpStatusCode.OK, loginResponse.StatusCode);
+        }
+
+        private void ClearDatabase()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+        }
+    }
+}
